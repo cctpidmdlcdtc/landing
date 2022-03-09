111539055c20d77c55f7f7ee65b14299aadb8b7a
cctpidmdlcdtc <cctpidmdlcdtc@gmail.com>
2022-03-09 23:10:11 +0100

    ignoramos la base de datos

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456790'
 
 # Create in-memory database
-app.config['DATABASE_FILE'] = 'sample_db.sqlite'
+app.config['DATABASE_FILE'] = 'cctpidmdlcdtc.sqlite'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
